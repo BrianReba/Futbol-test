f4f5776a68560bd45b7b5c8c4b1f1544a53724d8
BrianReba <brianezequiel.reba@gmail.com>
2024-01-08 21:07:03 -0300

    updated builder

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,26 @@
+//Bring data and create server actions
+//Here we link the client and server components. Import the CC and pass props
+
+import api from "@/lib/api"
+import BuilderPageClient from "./page-client"
+import { redirect } from "next/navigation"
+
+export default async function BuilderPage() {
+	const players = await api.player.list()
+
+	async function createTeams(formData: FormData) {
+		'use server'
+
+		const players = formData.getAll('player') //retrieve all values 'player' from the FormData object
+
+		const searchParams = new URLSearchParams()
+
+		for (const player of players) {
+			searchParams.append('players', player as string)
+		}
+			redirect(`/builder/match?${searchParams.toString()}`);
+		
+	}
+
+	return <BuilderPageClient onCreate={createTeams} players={players} />
+}
